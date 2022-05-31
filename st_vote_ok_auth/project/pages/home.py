f2bc202ae480d8ccf7ab55021957c11e2052c1e8
Jose Carlos Rocha <35368958+joserochabh@users.noreply.github.com>
2022-05-31 19:26:14 -0300

    Delete st_vote_ok_auth directory

--- st_vote_ok_auth/project/pages/home.py
+++
@@ -1,8 +0,0 @@
-# Load important libraries 
-import pandas as pd
-import streamlit as st 
-
-def app():
-    st.title("Controle de eleições")
-    
-    
--- st_vote_ok_auth/scripts/app_get_file.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import Lbs001
-
-st.text('Download de arquivo')
-st.markdown(Lbs001.get_file('apuracao1.docx', 'Documento de apuração'), unsafe_allow_html=True)
